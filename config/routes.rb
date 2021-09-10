a64a4db8e8b0a75760b5bbf8e9214d1024a10be5
ERIC CARRILLO <eric.rudolph.carrillo@gmail.com>
2021-09-10 13:34:36 -0700

    finished and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "signups#new"
+  get "thanks" => "pages#thanks"
+  resources :signups
+  post "/" => "signups#create"
 end
